c2380f2f33c6df5e112d02820c3187bfe44791c1
alfred cepeda <alfredjcepeda@gmail.com>
2022-04-22 14:42:17 -0400

    Adds bash scripts to run commands

---
+++ run_headless.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+docker run --rm -it -v $SCRIPT_DIR/src:/mnt/host/src -e WALLET_SEED -e WALLET_PASSWORD metamask npx ts-node src/browser/headless.ts
--- setup.sh
+++ setup.sh
@@ -3,4 +3,4 @@ apt-get install -y curl git vim zsh docker chromium xvfb
 curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh | zsh
 sed -i "s/ZSH_THEME=.*/ZSH_THEME=\"bira\"/" ~/.zshrc
 yarn install
-yarn download-metamask
+yarn fetch-metamask
